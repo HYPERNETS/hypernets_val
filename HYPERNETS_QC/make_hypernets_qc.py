008e2020e019baf3ce4c75a1f1f1cbef74893463
luiscnr <lois@LAPTOP-SU04997U>
2024-03-21 17:57:02 +0100

    Option CSVTONC for HYPERNETS QC

--- HYPERNETS_QC/make_hypernets_qc.py
+++ HYPERNETS_QC/make_hypernets_qc.py
@@ -351,7 +351,7 @@ def get_flag_array(indices, df, flags, flag):
         data_flag_array = np.array(data_flag).astype(np.int64)
     except:
         for idx in range(len(flags[flag]['values'])):
-            val = int((flags[flag]['values'][idx]).strip())
+            val = flags[flag]['values'][idx]
             meaning = flags[flag]['meanings'][idx].strip()
             data_flag[data_flag == meaning] = val
         data_flag_array = np.array(data_flag).astype(np.int64)
